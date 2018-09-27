1eb945572bcd413b1f0ed6180912faed8448768e
Vivek Varma <vsquared101@gmail.com>
2018-09-27 07:11:27 +0000

    Add Gradle based Spring Boot project to Git.

---
+++ src/main/java/com/learn/testingdemo/TestingdemoApplication.java
@@ -0,0 +1,12 @@
+package com.learn.testingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingdemoApplication.class, args);
+	}
+}
